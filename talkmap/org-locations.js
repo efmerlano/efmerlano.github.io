d5f08ea289b1480c34059168bffcfe583c7acadd
efmerlano <87490697+efmerlano@users.noreply.github.com>
2021-07-18 13:16:07 +0100

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,187 +1,22 @@
 var addressPoints = [
   [
-    "Cartagena, Colombia",
-    10.391049,
-    -75.479426
+    "Berkeley CA, USA",
+    37.8708393,
+    -122.2728638
   ],
   [
-    "Toronto, Canada",
-    43.653226,
-    -79.383184
-  ],
-  [
-    "Bogota D.C., Colombia",
-    4.598056,
-    -74.075833
-  ],
-  [
-    "London, England",
+    "London, UK",
     51.5073219,
     -0.1276473
   ],
   [
-    "Oslo, Norway",
-    59.913869,
-    10.752245
+    "San Francisco, California",
+    37.7792808,
+    -122.4192362
   ],
   [
-    "Stockholm, Sweden",
-    59.329323,
-    18.068581
-  ],
-  [
-    "Copenhagen, Denmark",
-    55.676097,
-    12.568337
-  ],
-  [
-    "Berlin, Germany",
-    52.520007,
-    13.404954
-  ],
-  [
-    "Prague, Czech Republic",
-    50.075538,
-    14.4378
-  ],
-  [
-    "Vienna, Austria",
-    48.208174,
-    16.373819
-  ],
-  [
-    "Venice, Italy",
-    45.440847,
-    12.315515
-  ],
-  [
-    "Florence, Italy",
-    43.76956,
-    11.255814
-  ],
-  [
-    "Rome, Italy",
-    41.902784,
-    12.496366
-  ],
-  [
-    "Edinburgh, Scotland",
-    55.953252,
-    -3.188267
-  ],
-  [
-    "Miami, USA",
-    25.76168,
-    -80.19179
-  ],
-  [
-    "Atlanta, USA",
-    33.748997,
-    -84.387985
-  ],
-  [
-    "Nashville, USA",
-    36.166340,
-    -86.779068
-  ],
-  [
-    "Panama, Panama",
-    8.982379,
-    -79.51987
-  ],
-  [
-    "Cali, Colombia",
-    3.451647,
-    -76.531985
-  ],
-  [
-    "Medellin, Colombia",
-    6.244203,
-    -75.581212
-  ],
-  [
-    "Popayan, Colombia",
-    2.45,
-    -76.616667
-  ],
-  [
-    "Cucuta",
-    7.894167,
-    -72.503889
-  ],
-  [
-    "Santiago, Chile",
-    -33.44889,
-    -70.669265
-  ],
-  [
-    "Vina del Mar, Chile",
-    -33.015348,
-    -71.550028
-  ],
-  [
-    "Valparaiso, Chile",
-    -33.047238,
-    -71.612688
-  ],
-  [
-    "Freeport, The Bahamas",
-    26.494235,
-    -78.681207
-  ],
-  [
-    "Great Abaco, The Bahamas",
-    26.355503,
-    -77.148516
-  ],
-  [
-    "Nassau, The Bahamas",
-    25.047984,
-    -77.355413
-  ],
-  [
-    "Bimini, The Bahamas",
-    25.7097,
-    -79.2996
-  ],
-  [
-    "Washington D.C., USA",
-    38.907192,
-    -77.036871
-  ],
-  [
-    "Sydney, Australia",
-    -33.867139,
-    151.207114
-  ],
-  [
-    "Canberra, Australia",
-    -35.280937,
-    149.130009
-  ],
-  [
-    "Paris, France",
-    48.856614,
-    2.352222
-  ],
-  [
-    "Madrid, Spain",
-    40.416775,
-    -3.70379
-  ],
-  [
-    "Toledo, Spain",
-    39.862832,
-    -4.027323
-  ],
-  [
-    "Barcelona, Spain",
-    41.385064,
-    2.173404
-  ],
-  [
-    "Cardiff, Wales",
-    51.481581,
-    -3.17909
+    "Los Angeles, CA",
+    34.0543942,
+    -118.2439408
   ]
 ];
